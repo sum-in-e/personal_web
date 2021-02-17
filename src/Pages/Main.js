d106a6fa3a451a2c26f38195d26495439ec2d6fb
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2021-02-17 23:39:58 +0900

    메인 페이지 기본 틀 완성

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,5 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+import Navigation from '../components/Navigation';
 
-const Main = () => <h1>Main</h1>;
+const Main = () => (
+	<Container>
+		<Navigation />
+		<Summary>
+			<TitleWrapper>
+				<Title>{`"Less is more."`}</Title>
+				<SemiTitle>쉽게 읽히는 서비스를 만들어가는 개발자 입니다.</SemiTitle>
+			</TitleWrapper>
+		</Summary>
+	</Container>
+);
+
+const Container = styled.main`
+	height: 100vh;
+	width: 70%;
+	padding: 30px 40px;
+	background-color: #e1e8ed;
+	background-color: #f3f6f8;
+`;
+
+const Summary = styled.section`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	height: 100%;
+
+	& > * {
+		width: 80%;
+	}
+`;
+
+const TitleWrapper = styled.div`
+	margin-bottom: 60px;
+`;
+
+const Title = styled.h1`
+	margin-bottom: 40px;
+	font-family: 'Newsreader', serif;
+	font-weight: 300;
+	font-size: 35px;
+	font-style: italic;
+`;
+
+const SemiTitle = styled.h2`
+	font-size: 20px;
+`;
 
 export default Main;
