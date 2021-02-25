7a2d93b9e27e85b87733aef1b41208503a3452d5
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2021-02-25 20:49:26 +0900

    포트폴리오 페이지 업데이트

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+import Navigation from '../components/Navigation';
+import Preview from '../components/Preview';
+import DataRepo from '../components/DataRepository';
 
-const Portfolio = () => <h1>Portfolio</h1>;
+const Portfolio = () => (
+	<Container>
+		<Navigation />
+		<Wrapper>
+			<PreviewList>
+				{DataRepo.map((data, index) => (
+					<Preview key={index} data={data} />
+				))}
+			</PreviewList>
+		</Wrapper>
+	</Container>
+);
+
+const Container = styled.main`
+	overflow-y: scroll;
+	width: 70%;
+	height: 100vh;
+	padding: 30px;
+	background-color: #f3f6f8;
+`;
+
+const Wrapper = styled.section`
+	padding: 60px 15%;
+`;
+
+const PreviewList = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
 
 export default Portfolio;
