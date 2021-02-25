8f49156ad45283f2e577aee359907b01234e1ad5
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2021-02-25 20:50:03 +0900

    Minor Edit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 import Router from './Router';
 import Header from './components/Header';
 import GlobalStyle from './components/GlobalStyle';
 
 const App = () => (
-	<>
+	<Container>
 		<GlobalStyle />
 		<Header />
 		<Router />
-	</>
+	</Container>
 );
+
+const Container = styled.div`
+	display: flex;
+	width: 100%;
+`;
 
 export default App;
