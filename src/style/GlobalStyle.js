a9cace0ef7adc9fe5ca5d410ffafe99c3bd66824
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2021-03-03 19:48:47 +0900

    GlobalStyle 설정

---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const globalStyle = createGlobalStyle`
+    ${reset};
+
+   @font-face {
+        font-family: 'ELAND_Choice_M';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts-20-12@1.0/ELAND_Choice_M.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    a{
+        text-decoration : none;
+        color : inherit;
+    }
+    *{
+        box-sizing : border-box;
+        word-break : keep-all;
+        word-wrap : break-word;
+    }
+    html, body{
+        overflow-x : hidden;
+        margin : 0;
+        font-family: 'NanumSquare', sans-serif;  
+        font-size : 18px;
+        line-height : 25px;
+        background-color : ${props => props.theme.white};
+    }
+`;
+
+export default globalStyle;
