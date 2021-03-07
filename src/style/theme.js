7ed0ee00fa975dc8993d37b3d697a1a7fe7c79e0
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2021-03-07 19:09:00 +0900

    반응형 BreakPoint 설정

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,10 +1,32 @@
+const size = {
+	// Small devices (landscape phones, 576px and up)
+	s: '576px',
+
+	// Medium devices (tablets, 768px and up)
+	m: '768px',
+
+	// Large devices (desktops, 992px and up)
+	l: '992px',
+
+	// X-Large devices (large desktops, 1200px and up)
+	xl: '1200px',
+
+	// XX-Large devices (larger desktops, 1400px and up)
+	xxl: '1400px',
+};
+
 const theme = {
 	main: '#fca311',
 	text: '#616161',
 	indigo: '#14213d',
 	black: '#272625',
 	white: '#ffffff',
-	line: '#868E96',
+	line: '#adb5bd',
+	s: `(min-width:${size.s})`,
+	m: `(min-width:${size.m})`,
+	l: `(min-width:${size.l})`,
+	xl: `(min-width:${size.xl})`,
+	xxl: `(min-width:${size.xxl})`,
 };
 
 export default theme;
