32a8517b3ea93f085b45a5c4902179fc92858aaa
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2021-03-07 19:08:43 +0900

    폰트 추가

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -3,7 +3,13 @@ import reset from 'styled-reset';
 
 const globalStyle = createGlobalStyle`
     ${reset};
-
+    @font-face {
+        font-family: 'NanumBarunGothic';
+        font-style: normal;
+        font-weight: 400;
+        src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.eot');
+        src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.eot?#iefix') format('embedded-opentype'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.woff') format('woff'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.ttf') format('truetype');
+    }
    @font-face {
         font-family: 'ELAND_Choice_M';
         src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts-20-12@1.0/ELAND_Choice_M.woff') format('woff');
@@ -17,7 +23,7 @@ const globalStyle = createGlobalStyle`
     }
     *{
         box-sizing : border-box;
-        word-break : keep-all;
+        word-break :normal;
         word-wrap : break-word;
     }
     html, body{
